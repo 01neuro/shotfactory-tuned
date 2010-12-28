142f8aa61d30dddab84acdb84fa8f2001e8436f8
czaks <marcin@6irc.net>
2010-12-28 09:02:05 +0100

    I have realized that it needs more time than I thought at first

--- sf-czaks.sh
+++ sf-czaks.sh
@@ -3,4 +3,4 @@ FACTORY=$1
 PASSWORD=$2
 NUMBER=$3
 PORT=$((5900+$3))
-exec python shotfactory.py -P $PASSWORD -d :$NUMBER -b $PORT -w 12 -l 8.0 -u 2048 -m 5 -v -f $FACTORY
+exec python shotfactory.py -P $PASSWORD -d :$NUMBER -b $PORT -w 22 -l 8.0 -u 2048 -m 5 -v -f $FACTORY
