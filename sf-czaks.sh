5fbd04edc305e7798d3f82a9378e0c5c0d59d36c
czaks <marcin@6irc.net>
2010-12-28 08:54:47 +0100

    Still, make it faster in The Right Way.

--- sf-czaks.sh
+++ sf-czaks.sh
@@ -3,4 +3,4 @@ FACTORY=$1
 PASSWORD=$2
 NUMBER=$3
 PORT=$((5900+$3))
-exec python shotfactory.py -P $PASSWORD -d :$NUMBER -b $PORT -w 30 -l 8.0 -u 2048 -m 2 -v -f $FACTORY
+exec python shotfactory.py -P $PASSWORD -d :$NUMBER -b $PORT -w 12 -l 8.0 -u 2048 -m 5 -v -f $FACTORY
