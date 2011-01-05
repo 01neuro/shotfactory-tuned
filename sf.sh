41716af237413281e80b93bf68bad89d7ca3c363
Michał Ludwiński <michal@designdivine.eu>
2011-01-05 21:40:12 +0100

    initial reusing commit (sf.sh updated for reusing)

--- sf.sh
+++ sf.sh
@@ -2,5 +2,16 @@
 PREFIX=$1
 PASSWORD=$2
 NUMBER=$3
+REUSE=$4
 PORT=$((5900+$3))
-exec python shotfactory.py -P $PASSWORD -d :$NUMBER -b $PORT -w 30 -l 8.0 -u 2048 -m 2 -v -f $PREFIX$NUMBER
+if [ -n "$REUSE" ]
+then
+echo "reusing enabled, vnc server will be reused $REUSE times"
+REUSE_CMD="-V 200"
+else
+REUSE_CMD=""
+fi
+RUN_CMD="python shotfactory.py -P $PASSWORD -d :$NUMBER -b $PORT -w 30 -l 8.0 -u 2048 -m 2 -v $REUSE_CMD -f $PREFIX$NUMBER"
+
+echo "exec $RUN_CMD"
+exec $RUN_CMD
